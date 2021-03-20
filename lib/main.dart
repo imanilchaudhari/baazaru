25ddf8f291a4345448dbb785c9bdcbec155f88ab
Anil Chaudhari <imanilchaudhari@gmail.com>
2021-03-21 01:25:55 +0545

    Copy files from flutter developers

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:baazaru/Constants/constants.dart';
+import 'package:baazaru/UI/Widgets/splash_screen.dart';
+import 'package:baazaru/UI/main_ui.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Classified App Clone',
+      theme: ThemeData(primaryColor: Colors.orange[200]),
+      routes: <String, WidgetBuilder>{
+        MAIN_UI: (BuildContext context) => MainUI(),
+        SPLASH_SCREEN: (BuildContext context) => AnimatedSplashScreen(),
+
+
+      },
+      initialRoute: SPLASH_SCREEN,
+    );
+  }
+}
