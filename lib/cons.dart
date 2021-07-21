4a06877124e787aa7bd5b671ac1ae0f853d64df1
Anil Chaudhari <imanilchaudhari@gmail.com>
2021-07-21 16:31:23 +0545

    WIP: constants with category id added

--- lib/cons.dart
+++ lib/cons.dart
@@ -22,3 +22,21 @@ const CATEGORY_PAGE = "category";
 
 const CREATE_PAGE = "create";
 const COMMENT_PAGE = "comment";
+const ACCOUNT_PAGE = "account";
+
+// category ids
+const CATEGORY_MOBI = 01; // mobiles & accessiories
+const CATEGORY_RSTT = 02; // real estate & constructions
+const CATEGORY_FASH = 04; // fashion & cloathings
+const CATEGORY_AUTO = 08; // autimotives
+const CATEGORY_BEAU = 11; // beauty & health
+const CATEGORY_BOOK = 17; // books & stationaries
+const CATEGORY_COMP = 20; // computer & periferials
+const CATEGORY_FURN = 23; // furniture & decore
+const CATEGORY_ACCE = 25; // Accessories
+const CATEGORY_SERV = 27; // services
+const CATEGORY_PETS = 28; // pets & animals
+const CATEGORY_JOBS = 29; // jobs
+const CATEGORY_ELEX = 53; // electronics & devices
+const CATEGORY_ENTE = 84; // Entertainment
+const CATEGORY_KITC = 150; // kitchen
